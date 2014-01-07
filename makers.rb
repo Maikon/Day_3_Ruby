dcf321ca6df1c9d4fa8b9a58a2732dc24a3643b6
Makis Otman <makisotman@gmail.com>
2014-01-07 18:30:53 +0000

    added another example of string interpolation

--- makers.rb
+++ makers.rb
@@ -3,3 +3,7 @@ puts "The people variable is currently: " + people
 
 people = "Coders" # this changes the value of the people variable
 puts "The people variable is currently #{people}" # this uses string interpolation
+
+ronin = 47
+puts "#{ronin} Ronin, avenged the murder of their master." # using string interpolation, I can convert a Fixnum into a String
+
