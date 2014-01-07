6e0e71bf28ec6fe9726386877dc68675a4d8b0c4
Makis Otman <makisotman@gmail.com>
2014-01-07 18:20:00 +0000

    Added string interpolation

--- makers.rb
+++ makers.rb
@@ -2,4 +2,4 @@ people = "Makers"
 puts "The people variable is currently: " + people
 
 people = "Coders" # this changes the value of the people variable
-puts "The people variable is currently " + people
+puts "The people variable is currently #{people}" # this uses string interpolation
